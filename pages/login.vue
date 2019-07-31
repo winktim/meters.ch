667098453f2bb74ee7557cc6a22ef0e95aa838d0
winktim <tfs.winkelmann@gmail.com>
2019-07-31 03:12:15 +0200

    Fixed translated text not showing without JavaScript

--- pages/login.vue
+++ pages/login.vue
@@ -2,7 +2,7 @@
   <div class="mt-4 section">
     <section class="section">
       <img src="/images/naito-900.svg" :alt="$t('alt.naito-one-logo')" class="w-32" />
-      <h1 class="font-heading text-2xl text-center" v-t="'pages.login.title'"></h1>
+      <h1 class="font-heading text-2xl text-center" v-text="$t('pages.login.title')"></h1>
     </section>
 
     <form
@@ -10,7 +10,7 @@
       action="#"
       @submit="login"
     >
-      <label class="w-full" for="email-input" v-t="'pages.login.form.email'"></label>
+      <label class="w-full" for="email-input" v-text="$t('pages.login.form.email')"></label>
       <input
         class="w-full px-4 py-2 transparent-input"
         type="email"
@@ -20,12 +20,12 @@
       />
 
       <div class="w-full pt-4 flex justify-between">
-        <label class="flex-grow" for="password-input" v-t="'pages.login.form.password'"></label>
+        <label class="flex-grow" for="password-input" v-text="$t('pages.login.form.password')"></label>
         <nuxt-link
           tabindex="1"
           to="/reset"
           class="green-link text-right pl-4"
-          v-t="'pages.login.form.forgotten-password'"
+          v-text="$t('pages.login.form.forgotten-password')"
         ></nuxt-link>
       </div>
       <input
@@ -37,7 +37,11 @@
       />
 
       <div class="w-full py-2 flex justify-end items-center">
-        <label class="select-none" for="remember-me-input" v-t="'pages.login.form.remember-me'"></label>
+        <label
+          class="select-none"
+          for="remember-me-input"
+          v-text="$t('pages.login.form.remember-me')"
+        ></label>
         <label class="material-checkbox text-naito-green-200" for="remember-me-input">
           <input type="checkbox" name="remember-me-input" id="remember-me-input" />
           <div class="material-checkbox-fake"></div>
@@ -53,7 +57,7 @@
     <a
       class="my-8 w-full action bg-naito-blue-200 text-gray-100 text-center"
       href="https://naito.one/meters"
-      v-t="'pages.login.new-client'"
+      v-text="$t('pages.login.new-client')"
     ></a>
   </div>
 </template>
