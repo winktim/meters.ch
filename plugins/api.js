f7be1c1422d945ea7835118e29c82b65c3978a4f
winktim <tfs.winkelmann@gmail.com>
2019-08-13 14:02:34 +0200

    Removed repetitions Fixed double put request for user

--- plugins/api.js
+++ plugins/api.js
@@ -1,39 +1,49 @@
 export default ({ app, store, redirect }, inject) => {
+  function classic(method, route, params) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const awaitingEvent = app['$' + method](route, params)
+        store.dispatch('addAwaitingEvent', { awaitingEvent })
+
+        const res = await awaitingEvent
+        const parsed = await res.json()
+
+        if (!res.ok) {
+          store.dispatch('showMessage', {
+            message: parsed.message,
+            isError: true,
+          })
+
+          if (res.status === 401 || res.status === 403) {
+            store.dispatch('logout')
+            redirect('/login')
+          }
+
+          reject(res.status)
+        }
+
+        resolve(parsed)
+      } catch (e) {
+        store.dispatch('showMessage', {
+          message: app.i18n.t('error.unknown'),
+          isError: true,
+        })
+
+        console.error(e)
+        reject(e)
+      }
+    })
+  }
+
   inject('getUser', async function(force) {
     if (store.state.data.user !== null && !force) {
       return
     }
 
     try {
-      const awaitingEvent = app.$get('/users')
-      store.dispatch('addAwaitingEvent', { awaitingEvent })
-
-      const res = await awaitingEvent
-      const parsed = await res.json()
-
-      if (!res.ok) {
-        store.dispatch('showMessage', {
-          message: parsed.message,
-          isError: true,
-        })
-
-        if (res.status === 401 || res.status === 403) {
-          store.dispatch('logout')
-          redirect('/login')
-        }
-
-        return
-      }
-
+      const parsed = await classic('get', '/users')
       store.commit('SET_USER', { user: parsed[0] })
-    } catch (e) {
-      store.dispatch('showMessage', {
-        message: app.i18n.t('error.unknown'),
-        isError: true,
-      })
-
-      console.error(e)
-    }
+    } catch (e) {}
   })
 
   inject('getResources', async function(force) {
@@ -42,35 +52,20 @@ export default ({ app, store, redirect }, inject) => {
     }
 
     try {
-      const awaitingEvent = app.$get('/resources')
-      store.dispatch('addAwaitingEvent', { awaitingEvent })
-
-      const res = await awaitingEvent
-      const parsed = await res.json()
-
-      if (!res.ok) {
-        store.dispatch('showMessage', {
-          message: parsed.message,
-          isError: true,
-        })
-
-        if (res.status === 401 || res.status === 403) {
-          store.dispatch('logout')
-          redirect('/login')
-        }
-
-        return
-      }
-
+      const parsed = await classic('get', '/resources')
       store.commit('SET_RESOURCES', { resources: parsed })
-    } catch (e) {
-      store.dispatch('showMessage', {
-        message: app.i18n.t('error.unknown'),
-        isError: true,
-      })
+    } catch (e) {}
+  })
 
-      console.error(e)
+  inject('getResourceTypes', async function(force) {
+    if (store.state.data.resourceTypes !== null && !force) {
+      return
     }
+
+    try {
+      const parsed = await classi('get', '/resourceTypes')
+      store.commit('SET_RESOURCE_TYPES', { resourceTypes: parsed })
+    } catch (e) {}
   })
 
   inject('getSites', async function(force) {
@@ -79,35 +74,9 @@ export default ({ app, store, redirect }, inject) => {
     }
 
     try {
-      const awaitingEvent = app.$get('/sites')
-      store.dispatch('addAwaitingEvent', { awaitingEvent })
-
-      const res = await awaitingEvent
-      const parsed = await res.json()
-
-      if (!res.ok) {
-        store.dispatch('showMessage', {
-          message: parsed.message,
-          isError: true,
-        })
-
-        if (res.status === 401 || res.status === 403) {
-          store.dispatch('logout')
-          redirect('/login')
-        }
-
-        return
-      }
-
+      const parsed = await classic('get', '/sites')
       store.commit('SET_SITES', { sites: parsed })
-    } catch (e) {
-      store.dispatch('showMessage', {
-        message: app.i18n.t('error.unknown'),
-        isError: true,
-      })
-
-      console.error(e)
-    }
+    } catch (e) {}
   })
 
   inject('getObjectives', async function(force) {
@@ -116,35 +85,9 @@ export default ({ app, store, redirect }, inject) => {
     }
 
     try {
-      const awaitingEvent = app.$get('/objectives')
-      store.dispatch('addAwaitingEvent', { awaitingEvent })
-
-      const res = await awaitingEvent
-      const parsed = await res.json()
-
-      if (!res.ok) {
-        store.dispatch('showMessage', {
-          message: parsed.message,
-          isError: true,
-        })
-
-        if (res.status === 401 || res.status === 403) {
-          store.dispatch('logout')
-          redirect('/login')
-        }
-
-        return
-      }
-
+      const parsed = await classic('get', '/objectives')
       store.commit('SET_OBJECTIVES', { objectives: parsed })
-    } catch (e) {
-      store.dispatch('showMessage', {
-        message: app.i18n.t('error.unknown'),
-        isError: true,
-      })
-
-      console.error(e)
-    }
+    } catch (e) {}
   })
 
   inject('getClient', async function(force) {
@@ -153,35 +96,9 @@ export default ({ app, store, redirect }, inject) => {
     }
 
     try {
-      const awaitingEvent = app.$get('/clients')
-      store.dispatch('addAwaitingEvent', { awaitingEvent })
-
-      const res = await awaitingEvent
-      const parsed = await res.json()
-
-      if (!res.ok) {
-        store.dispatch('showMessage', {
-          message: parsed.message,
-          isError: true,
-        })
-
-        if (res.status === 401 || res.status === 403) {
-          store.dispatch('logout')
-          redirect('/login')
-        }
-
-        return
-      }
-
+      const parsed = await classic('get', '/clients')
       store.commit('SET_CLIENT', { client: parsed[0] })
-    } catch (e) {
-      store.dispatch('showMessage', {
-        message: app.i18n.t('error.unknown'),
-        isError: true,
-      })
-
-      console.error(e)
-    }
+    } catch (e) {}
   })
 
   inject('putUser', async function(payload) {
@@ -190,28 +107,11 @@ export default ({ app, store, redirect }, inject) => {
     }
 
     try {
-      const awaitingEvent = app.$put(
+      const parsed = await classic(
+        'put',
         `/users/${store.state.data.user.id}`,
         payload
       )
-      store.dispatch('addAwaitingEvent', { awaitingEvent })
-
-      const res = await app.$put(`/users/${store.state.data.user.id}`, payload)
-      const parsed = await res.json()
-
-      if (!res.ok) {
-        store.dispatch('showMessage', {
-          message: parsed.message,
-          isError: true,
-        })
-
-        if (res.status === 401 || res.status === 403) {
-          store.dispatch('logout')
-          redirect('/login')
-        }
-
-        return
-      }
 
       // new user infos are returned on success
       store.commit('SET_USER', { user: parsed })
@@ -220,13 +120,6 @@ export default ({ app, store, redirect }, inject) => {
         message: app.i18n.t('api.user_updated'),
         isError: false,
       })
-    } catch (e) {
-      store.dispatch('showMessage', {
-        message: app.i18n.t('error.unknown'),
-        isError: true,
-      })
-
-      console.error(e)
-    }
+    } catch (e) {}
   })
 }
