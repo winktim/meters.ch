026152f6420314cf50aa31db4c656e3e55b53439
winktim <tfs.winkelmann@gmail.com>
2020-02-17 19:00:49 +0100

    Added loading state when changing page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="background-image font-content w-screen h-screen flex overflow-x-hidden">
+  <div
+    class="background-image font-content w-screen h-screen flex overflow-x-hidden"
+  >
     <loading-bar :show="isAppLoading"></loading-bar>
     <div class="absolute right-0 top-0">
       <language-selector
@@ -18,8 +20,15 @@
       v-if="isIE && !hideIEWarning"
       class="fixed bottom-0 left-0 w-full bg-orange-300 text-gray-900 text-center flex flex-col"
     >
-      <i @click="hideIEWarning = true" class="material-icons clickable w-full p-4">close</i>
-      <span class="p-4 pt-0" v-html="$t('global.browser_alert', { classes: 'green-link'})"></span>
+      <i
+        @click="hideIEWarning = true"
+        class="material-icons clickable w-full p-4"
+        >close</i
+      >
+      <span
+        class="p-4 pt-0"
+        v-html="$t('global.browser_alert', { classes: 'green-link' })"
+      ></span>
     </div>
   </div>
 </template>
@@ -54,6 +63,34 @@ export default {
       return this.$store.state.isIE
     },
   },
+  mounted() {
+    let navFrom = null
+    let navTo = null
+    let resolveFunc = null
+
+    this.$router.beforeEach((to, from, next) => {
+      if (to.name !== from.name) {
+        navFrom = from.name
+        navTo = to.name
+
+        this.$store.dispatch('addAwaitingEvent', {
+          awaitingEvent: new Promise(resolve => {
+            resolveFunc = resolve
+          }),
+        })
+      }
+      next()
+    })
+
+    this.$router.afterEach((to, from) => {
+      if (to.name === navTo && from.name === navFrom) {
+        navFrom = null
+        navTo = null
+        if (resolveFunc) {
+          resolveFunc()
+        }
+      }
+    })
+  },
 }
 </script>
-
