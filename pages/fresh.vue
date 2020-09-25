48a9e8c4d63c7447d83410f8624a09509db6f13e
winktim <twinkelmann@protonmail.com>
2020-09-25 18:32:50 +0200

    Added remember me after password reset

--- pages/fresh.vue
+++ pages/fresh.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="mt-4 lg:mt-16 flex flex-col items-center">
     <section class="flex flex-col items-center">
-      <img src="/images/naito-900.svg" :alt="$t('alt.naito_one_logo')" class="w-16 sm:mb-2 md:w-32" />
-      <h1 class="font-heading text-2xl text-center" v-text="$t('pages.fresh.title')"></h1>
+      <img
+        src="/images/naito-900.svg"
+        :alt="$t('alt.naito_one_logo')"
+        class="w-16 sm:mb-2 md:w-32"
+      />
+      <h1
+        class="font-heading text-2xl text-center"
+        v-text="$t('pages.fresh.title')"
+      ></h1>
     </section>
 
     <form
@@ -10,8 +17,15 @@
       action="#"
       @submit="reset"
     >
-      <p class="text-center text-gray-800 mb-4" v-text="$t('pages.fresh.form.explain')"></p>
-      <label class="w-full" for="password-input" v-text="$t('pages.login.form.password')"></label>
+      <p
+        class="text-center text-gray-800 mb-4"
+        v-text="$t('pages.fresh.form.explain')"
+      ></p>
+      <label
+        class="w-full"
+        for="password-input"
+        v-text="$t('pages.login.form.password')"
+      ></label>
       <input
         :disabled="resetting"
         class="w-full px-4 py-2 mb-8 transparent-input"
@@ -20,15 +34,43 @@
         id="password-input"
         required
       />
-      <label class="w-full" for="confirm-input" v-text="$t('pages.fresh.form.confirm')"></label>
+      <label
+        class="w-full"
+        for="confirm-input"
+        v-text="$t('pages.fresh.form.confirm')"
+      ></label>
       <input
         :disabled="resetting"
-        class="w-full px-4 py-2 mb-8 transparent-input"
+        class="w-full px-4 py-2 transparent-input"
         type="password"
         name="confirm-input"
         id="confirm-input"
         required
       />
+
+      <div class="w-full py-2 flex justify-end items-center">
+        <label
+          class="select-none"
+          for="remember-me-input"
+          v-text="$t('pages.login.form.remember_me')"
+        ></label>
+        <label
+          class="material-checkbox text-naito-green-200"
+          for="remember-me-input"
+        >
+          <input
+            :disabled="resetting"
+            type="checkbox"
+            name="remember-me-input"
+            id="remember-me-input"
+            v-model="rememberMe"
+          />
+          <div
+            class="material-checkbox-fake material-checkbox-fake__large"
+          ></div>
+        </label>
+      </div>
+
       <input
         :disabled="resetting"
         class="text-center action w-full bg-naito-green-200 text-gray-100"
@@ -56,14 +98,44 @@ export default {
       ],
     }
   },
-  mounted() {
-    this.verifyToken = this.$route.query.verify_token
-  },
   data() {
     return {
       verifyToken: null,
       resetting: false,
+      rememberMe: false,
     }
+  },
+  mounted() {
+    this.verifyToken = this.$route.query.verify_token
+    this.rememberMe = this.$route.query.hasOwnProperty('remember-me')
+  },
+  watch: {
+    rememberMe(to, from) {
+      if (to === from) {
+        return
+      }
+
+      const route = {
+        query: {
+          verify_token: this.verifyToken,
+        },
+      }
+
+      if (to) {
+        route.query['remember-me'] = null
+      }
+      this.$router.replace(
+        route,
+        () => {},
+        e => {
+          // ignore this error because it is not important
+          if (e === undefined || e.name === 'NavigationDuplicated') {
+            return
+          }
+          console.error(e)
+        }
+      )
+    },
   },
   methods: {
     async reset(event) {
@@ -72,6 +144,7 @@ export default {
       this.resetting = true
       const password = event.target[0].value
       const password_confirmation = event.target[1].value
+      const rememberMe = event.target[2].checked
 
       // hide a potential message
       this.$store.dispatch('hideMessage')
@@ -108,6 +181,10 @@ export default {
           return
         }
 
+        if (rememberMe) {
+          this.$store.commit('SET_REMEMBER_ME', { rememberMe })
+        }
+
         localStorage.setItem('hasConnected', true)
         this.$store.commit('SET_API_TOKEN', { apiToken: parsed.api_token })
 
@@ -126,4 +203,3 @@ export default {
   },
 }
 </script>
-
